33cb3dda9f12e7f7ebee09b0c285c74e7d8edb5c
Invectorgator <173741409+InvectorGator@users.noreply.github.com>
2024-11-13 10:21:26 -0500

    Vite config fix for client url variables; host by default.

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,21 +4,26 @@ import { quasar, transformAssetUrls } from '@quasar/vite-plugin';
 import esLintPlugin from 'vite-plugin-eslint';
 
 export default defineConfig(({ mode }) => {
-    // Configure the API and Vite paths.
+    // Configure the API and Vite paths...
     const ENV = loadEnv(mode, process.cwd(), '');
+    // Determine the API (server) base path Vite will connect to.
     const API_PORT = ENV.VITE_API_PORT === '' ? '' : ENV.VITE_API_PORT === undefined ? 5025 : ENV.VITE_API_PORT;
-    const API_PATH = ENV.VITE_API_BASE_URL || "http://localhost" + (API_PORT === '' ? '' : ":" + API_PORT);
+    const API_PATH_WITH_PORT = (ENV.VITE_API_BASE_URL || "http://localhost") + (API_PORT === '' ? '' : ":" + API_PORT);
+    // Determine the Vite client settings.
     const VITE_PORT = ENV.VITE_PORT === '' ? '' : ENV.VITE_PORT === undefined ? 5026 : ENV.VITE_PORT;
+    const VITE_BASE = ENV.VITE_BASE || "/";
+    const VITE_RUN_AS_HOST = !(Boolean(ENV.VITE_RUN_AS_HOST) === false);
+    // Display purposes only - determine the Vite client URL to display.
     const VITE_PATH = ENV.VITE_BASE_URL || "http://localhost";
 
     // Log paths to console for visibility.
     console.log("Now starting WhereIsMyStuff: ");
-    console.log(`   Client Path:    ${VITE_PATH}${VITE_PORT === '' ? '' : `:${VITE_PORT}`}`);
-    console.log(`   API Path:       ${API_PATH}`);
+    console.log(`   Client Path:    ${VITE_PATH}${VITE_PORT === '' ? '' : `:${VITE_PORT}`}${VITE_BASE}`);
+    console.log(`   API Path:       ${API_PATH_WITH_PORT}`);
 
     // Configure Vite
     return {
-        base: VITE_PATH,
+        base: VITE_BASE,
         plugins: [
             vue({
                 template: { transformAssetUrls }
@@ -31,9 +36,10 @@ export default defineConfig(({ mode }) => {
         ],
         server: {
             port: VITE_PORT,
+            host: VITE_RUN_AS_HOST,
             proxy: {
                 '/api': {
-                    target: API_PATH,
+                    target: API_PATH_WITH_PORT,
                     changeOrigin: true
                 }
             }
